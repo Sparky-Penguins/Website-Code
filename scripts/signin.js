041434ca4104dd1e8c74dd6376418ad230b00ee3
NateDCoder <rubyandtwixie@gmail.com>
2025-03-11 18:47:57 -0400

    checking to see if it works on github

--- scripts/signin.js
+++ scripts/signin.js
@@ -1,8 +1,15 @@
-async function handleCredentialResponse(response) {
-    const token = response.credential;
+window.handleCredentialResponse = (response) => {
+  // const token = response.credential;
 
-    // Send token to backend for verification
-    const res = await fetch('http://localhost:3000/auth/google?token=' + token);
-    const data = await res.json();
-    console.log('User Info:', data);
-}
+  // // Send token to backend for verification
+  // decodeJwtResponse() is a custom function defined by you
+  // to decode the credential response.
+  responsePayload = decodeJwtResponse(response.credential);
+
+  console.log("ID: " + responsePayload.sub);
+  console.log("Full Name: " + responsePayload.name);
+  console.log("Given Name: " + responsePayload.given_name);
+  console.log("Family Name: " + responsePayload.family_name);
+  console.log("Image URL: " + responsePayload.picture);
+  console.log("Email: " + responsePayload.email);
+};
