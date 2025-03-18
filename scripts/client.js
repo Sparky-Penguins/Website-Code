71e983c9c23cebfef1893af8b56f9a76966fe2e4
NateDCoder <rubyandtwixie@gmail.com>
2025-03-18 19:49:34 -0400

    working product page

--- scripts/client.js
+++ scripts/client.js
@@ -1,17 +1,48 @@
-fetch(
-  "https://rational-mastiff-model.ngrok-free.app/hello",
-  {
-    method: "GET",
-    headers: {
-      "ngrok-skip-browser-warning": "true",
-    },
-  }
-)
+fetch("https://rational-mastiff-model.ngrok-free.app/products", {
+  method: "GET",
+  headers: {
+    "ngrok-skip-browser-warning": "true",
+  },
+})
   .then((response) => response.text()) // Get the text from the response
   .then((data) => {
-    console.log(data);
-    // Display the response in the div with id 'response'
-    alert("This is hello from the server");
+    products = JSON.parse(data);
+    const shopPage = document.getElementById("shop-page");
+
+    products.forEach((product) => {
+      // Create the service container
+      const serviceDiv = document.createElement("div");
+      serviceDiv.classList.add("service");
+
+      // Create the product name
+      const h2 = document.createElement("h2");
+      h2.textContent = product.PRODUCT;
+
+      // Create the description
+      const p = document.createElement("p");
+      p.textContent = product.ITEM_DESCRIPTION;
+
+      // Create the price
+      const priceDiv = document.createElement("div");
+      priceDiv.classList.add("price");
+      priceDiv.textContent = product.PRICE;
+
+      // Create the "Request Now" button
+      const btn = document.createElement("a");
+      btn.href = "#";
+      btn.classList.add("btn");
+      btn.textContent = "Request Now";
+
+      // Append all elements to the service container
+      serviceDiv.appendChild(h2);
+      serviceDiv.appendChild(p);
+      serviceDiv.appendChild(priceDiv);
+      serviceDiv.appendChild(btn);
+
+      // Append the service container to the shop page
+      shopPage.appendChild(serviceDiv);
+      console.log(serviceDiv)
+    });
   })
   .catch((error) => {
     // Handle any errors
